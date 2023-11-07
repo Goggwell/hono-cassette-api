671f0224469ca84a31f4513581c528cbe3fa2ff3
Goggwell <zulkarnaensamuel1@gmail.com>
2023-11-08 04:43:03 +0700

    feat: implement KV

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { Pool } from '@neondatabase/serverless';
 import { beasts } from './db/schema';
 import { Hono } from 'hono';
 import { cache } from 'hono/cache';
+import { cacheData, getCachedData } from './utils';
 
 export type Env = {
 	DATABASE_URL: string;
+	hcbkv: KVNamespace;
 };
 
 const app = new Hono<{ Bindings: Env }>();
@@ -21,13 +23,32 @@ app.get(
 
 app.get('/', async (c) => {
 	try {
-		const client = new Pool({ connectionString: c.env.DATABASE_URL });
-		const db = drizzle(client);
-		const result = await db.select().from(beasts);
+		const data = await getCachedData(c.env.hcbkv);
 
 		return c.json({
-			result,
+			data,
 		});
+	} catch (error) {
+		console.error(error);
+		return c.json(
+			{
+				error,
+			},
+			400
+		);
+	}
+});
+
+app.get('/update', async (c) => {
+	// const client = new Pool({ connectionString: c.env.DATABASE_URL });
+	// const db = drizzle(client);
+	// const response = await db.select().from(beasts);
+	// console.log('response');
+	// await c.env.hcbkv.put('BEASTS', JSON.stringify(response)).then(() => console.log('works!'));
+	try {
+		const client = new Pool({ connectionString: c.env.DATABASE_URL });
+		const db = drizzle(client);
+		return await cacheData(db, c.env.hcbkv);
 	} catch (error) {
 		console.error(error);
 		return c.json(
---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+import { NeonDatabase } from 'drizzle-orm/neon-serverless';
+import { beasts } from './db/schema';
+
+export async function cacheData(db: NeonDatabase<Record<string, never>>, namespace: KVNamespace) {
+	const response = await db.select().from(beasts);
+	return await namespace.put('BEASTS', JSON.stringify(response)).then(() => new Response('Data successfully cached!', { status: 201 }));
+}
+
+export async function getCachedData(namespace: KVNamespace) {
+	const response = await namespace.get('BEASTS');
+	if (!response) return new Response("Couldn't get data", { status: 404 });
+	return JSON.parse(response);
+}
